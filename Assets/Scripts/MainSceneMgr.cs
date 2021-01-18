52eb6ee8b21fafbc0df2cddf37fbc17b68a45a10
wonAdam <ehdgus5500@gmail.com>
2021-01-18 13:01:49 +0900

    main scene & build

---
+++ Assets/Scripts/MainSceneMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneMgr : MonoBehaviour
+{
+    public void OnClick_Btn(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/ToMainBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMainBtn : MonoBehaviour
+{
+    public void OnClick_ToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
